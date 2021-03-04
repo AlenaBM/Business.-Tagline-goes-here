8ad8891282345a28b8fc2f0e5fa6e5e7c13dcf30
AlenaBM <9642444600a@gmail.com>
2021-03-04 21:05:08 +0300

    Added alert in button

---
+++ js.js
@@ -0,0 +1,10 @@
+var modal = $modal(
+  {
+  title: 'Текст заголовка',
+  content: '<p>Содержимое модального окна...</p>',
+  footerButtons: [
+    { class: 'btn btn__cancel', text: 'Отмена', handler: 'modalHandlerCancel' },
+    { class: 'btn btn__ok', text: 'ОК', handler: 'modalHandlerOk' }
+  ]
+}
+);
